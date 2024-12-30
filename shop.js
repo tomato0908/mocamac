b03b9d04c1f6e28bbccbec08de86a5d4e3031ee2
ooeichiro <228428@st.takushoku-u.ac.jp>
2024-12-30 17:27:13 +0900

    news.json更新

--- shop.js
+++ shop.js
@@ -1,7 +1,7 @@
 const items = document.getElementById("items");
 
 // JSONファイルを読み込む
-fetch('news.json')
+fetch('https://tomato0908.github.io/mocamac/news.json')
     .then(response => {
         if (!response.ok) {
             throw new Error('JSONファイルの読み込みに失敗しました');
@@ -9,17 +9,25 @@ fetch('news.json')
         }
         return response.json(); // JSONデータを解析
     })
+
+
     .then(data => {
         // ニュースデータをループしてHTMLに追加
       
         data.forEach(news => {
             // ニュースを表示するHTMLを作成
             const newsItem = `
-                <div>
-                    <h3>${news.date} - ${news.title}</h3>
-                    <p>${news.content}</p>
-                    <hr>
-                </div>
+        <table id="shoptable" class="shoptable">
+            <tr>
+                <th>商品名</th>
+            </tr>
+            <tr>
+                <td><img src="image01.jpg" class="img"></td>
+            </tr>
+            <tr>
+                <td>値段</td>
+            </tr>
+        </table>
             `;
             items.innerHTML += newsItem;
         });
